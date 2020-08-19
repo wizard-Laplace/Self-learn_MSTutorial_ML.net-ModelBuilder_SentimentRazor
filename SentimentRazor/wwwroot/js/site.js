91295d99701621176f5d927c9ace1b82a14d3d07
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-08-19 16:45:23 +0900

    javascriptによる動的処理を定義

--- SentimentRazor/wwwroot/js/site.js
+++ SentimentRazor/wwwroot/js/site.js
@@ -2,3 +2,41 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+// OnGetAnalyzeSentiment ハンドラーへのユーザー入力を使用して GET HTTP要求を作成
+function getSentiment(userInput) {
+    return fetch(`Index?handler=AnalyzezSentiment&text=${userInput}`)
+        .then((response) => {
+            return response.text();
+        })
+}
+
+// センチメントが予測されると、Webページ上のマーカーの位置を動的に更新
+function updateMarker(markerPosition, sentiment) {
+    $("#markerPosition").attr("style", `left:${markerPosition}%`);
+    $("#markerValue").text(sentiment);
+}
+
+// ユーザーから入力を取得し、getSentiment 関数を使用してそれを OnGetAnalyzeSentiment 関数に送信し、
+// updateMarker 関数でマーカーを更新
+function updateSentiment() {
+    var userInput = $("#Message").val();
+
+    getSentiment(userInput)
+        .then((entiment) => {
+            switch (sentiment) {
+                case "Not Toxic":
+                    updateMarker(100.0, sentiment);
+                    break;
+                case "Toxic":
+                    updateMarker(0.0, sentiment);
+                    break;
+                default:
+                    updateMarker(45.0, "Neutral");
+            }
+        });
+}
+
+// イベントハンドラーを登録し、id=Message 属性を使ってtextarea要素にバインド
+$("#Message").on('change input paste', updateSentiment)
